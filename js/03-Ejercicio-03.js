de4ff7f765aee9dc9341fdfe74f6c54867f7b65b
Cristian Brandan <cristian_brandan@hotmail.com>
2025-04-10 19:01:40 -0300

    Resolución del ejercicio nº 3

---
+++ js/03-Ejercicio-03.js
@@ -0,0 +1,33 @@
+/*
+
+Sumar el resultado de lanzar dos dados 
+y anotar en un array el número de apariciones de dicha suma
+repitiendo 50 veces esta operación.
+
+*/
+
+let dado1;
+let dado2;
+let suma;
+let apariciones = [];
+
+for (let i = 1; i <= 50; i++) {
+    dado1 = Math.floor(Math.random() * 6 + 1);
+    dado2 = Math.floor(Math.random() * 6 + 1);
+    
+    suma = dado1 + dado2;
+
+    apariciones.push(suma);
+}
+
+console.log(apariciones);
+
+document.write(`<table><thead><tr><th>Suma</th><th>Apariciones</th></tr></thead><tbody>`);
+
+for (let j = 2; j <= 12; j++) {
+    document.write(`<tr><td>${j}</td>`);
+    const repeticiones = apariciones.filter(numero => numero == j);
+    document.write(`<td>${repeticiones.length}</td></tr>`);
+}
+
+document.write(`</tbody></table>`);
